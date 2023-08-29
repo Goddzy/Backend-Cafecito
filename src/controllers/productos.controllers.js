9f382a20b9489eec8880399777c6ba2f5de8044d
Goddzy <renatotrevisiolmontiel@gmail.com>
2023-08-29 17:13:11 -0300

    agregué la lógica para listar productos y devolver un arreglo

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,6 +1,14 @@
 import Producto from "../models/producto";
-export const listarProductos = (req, res) => {
-  res.send("esto es una prueba de una petición get");
+export const listarProductos = async (req, res) => {
+  try {
+    //buscar en la BD la collection de los productos
+    const productos = await Producto.find();
+    //envio la respuesta al frontend
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({mensaje:'Error al buscar los productos'})
+  }
 };
 export const crearProducto = async (req, res) => {
   try {
@@ -9,9 +17,11 @@ export const crearProducto = async (req, res) => {
     //cuando los datos sean correctos, guardar el objeto en la BD
     const productoNuevo = new Producto(req.body);
     await productoNuevo.save(); //el save lo guarda en la BS que está conectada en el backend
-    res.status(201).json({mensaje: 'el producto fue creado correctamente'})
+    res.status(201).json({ mensaje: "el producto fue creado correctamente" });
   } catch (error) {
     console.log(error);
-    res.status(404).json({mensaje: 'ocurrio un error al intentar agregar un producto'})
+    res
+      .status(404)
+      .json({ mensaje: "ocurrio un error al intentar agregar un producto" });
   }
 };
