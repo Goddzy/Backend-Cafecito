2d4925dc3f34acbd71641c037a792921379f7696
Goddzy <renatotrevisiolmontiel@gmail.com>
2023-08-30 19:12:46 -0300

    agregamos la conexion a la BD de producción

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
-const url = 'mongodb://127.0.0.1:27017/cafecito'; //el local host se puede escribir como 127.0.0.1
+// const url = 'mongodb://127.0.0.1:27017/cafecito'; //el local host se puede escribir como 127.0.0.1 (base de dato local)
+const url = 'mongodb+srv://minitrevi2:IOj8Xwzz8cscnbqb@cluster0.p3adzae.mongodb.net/cafecito'; //(cadena de conexión a mongoAtlas)
 
 mongoose.connect(url);
 
